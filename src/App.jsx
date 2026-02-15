c2c6112085ee51ab36edf0896447bb2456049213
ashish singh <ashishsingh094562@gmail.com>
2026-02-15 12:36:57 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+  process.env.REACT_APP_API_BASE_URL;
 
 const App = () => {
   const [form, setForm] = useState({
